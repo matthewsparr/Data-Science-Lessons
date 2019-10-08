39bdf4ec2dd087b27deca4a4effd4c848e160be8
Matt <matthewsparr247@gmail.com>
2019-10-08 13:25:16 -0400

    Add files via upload

---
+++ APIs/secrets.py
@@ -0,0 +1,12 @@
+# ExampleTokenDemo-20190415
+class Lifx():
+    tokens = {
+        'lifx' : {
+            'token_name': 'Lifx',
+            'token': 'c33cf42e79aaf8afc8b647e13b07ff9fe668587c41c722ae6896462f835190ab',
+            'expired': False
+        }
+    }
+    
+    # Default token
+    default_token = tokens['lifx']
